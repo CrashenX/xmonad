ca442034c4a96defa54946ebe0c47195e2b797a8
Rebecca Skinner <rebecca@rebeccaskinner.net>
2011-09-15 23:16:15 -0500

    Updated logic for mapping keys

--- xmonad.hs
+++ xmonad.hs
@@ -7,22 +7,20 @@ import XMonad.Layout.Named
 import XMonad.Layout.Reflect
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.WindowNavigation
+import XMonad.Util.CustomKeys
 import System.IO
+import Control.Monad
 
-rzTall   = ResizableTall 1 (3/100) (1/2) []
-myLayout = Full
-           ||| named "Tall"     rzTall
-           ||| named "Mirror" (reflectHoriz rzTall)
-           ||| named "Wide"   (Mirror rzTall)
 
--- removeKeys has a bug that causes it to only remove the first key in the
--- list. This function works around that, it should be removed once the
--- arch package is updated with the bugfix.
-removeKeysFixed :: XConfig a -> [(ButtonMask,KeySym)] -> XConfig a
-removeKeysFixed xconf [] = xconf
-removeKeysFixed xconf keys = foldr (\x y -> y `removeKeys` [x]) xconf keys
+-- define layouts
+rzTall     = ResizableTall 1 (3/100) (1/2) []
+fullLayout = named "Full" Full
+tallLayout = named "Tall" rzTall
+mirrorTall = named "Mirror" $ reflectHoriz rzTall
+wideLayout = named "Wide" $ Mirror rzTall
 
--- pattern matching seems broken here, so use guards instead
+myLayout = fullLayout ||| tallLayout ||| mirrorTall ||| wideLayout
+
 vimDir k
     | k == xK_h = L
     | k == xK_j = D
@@ -35,12 +33,33 @@ vimResizeMap k
     | k == xK_k = sendMessage $ MirrorExpand
     | k == xK_l = sendMessage $ Expand
 
+-- vimResizeMapMirror k
+--     | k == xK_h = sendMessage $ MirrorShrink
+--     | k == xK_j = sendMessage $ Shrink
+--     | k == xK_k = sendMessage $ Expand
+--     | k == xK_l = sendMessage $ MirrorExpand
+
+vimResizeMapMirror k
+    | k == xK_h = sendMessage $ Expand
+    | k == xK_j = sendMessage $ Expand
+    | k == xK_k = sendMessage $ Expand
+    | k == xK_l = sendMessage $ Expand
+
+compareLayouts l1 l2 = (description l1) == (description l2)
+
+getVimResizeMap l
+    | (compareLayouts (layoutHook l) mirrorTall) = vimResizeMapMirror
+    | otherwise = vimResizeMap
+
 -- List of the bound keys for convenience
 vimKeys = [xK_h, xK_j, xK_k, xK_l]
 
-keysList = [((customShiftMask,x),sendMessage $ Swap (vimDir x))|x<-vimKeys]++
-           [((customCtrlMask ,x),vimResizeMap x) | x <- vimKeys] ++
-           [((mod4Mask       ,x),sendMessage $ Go (vimDir x)) | x <- vimKeys]
+deleteList _ = [(i,j)|i<-[mod4Mask,customShiftMask,customCtrlMask],j<-vimKeys]
+
+keysList l = 
+       [((customShiftMask,x),sendMessage $ Swap (vimDir x))|x<-vimKeys]
+    ++ [((customCtrlMask ,x),(getVimResizeMap l) x) | x <- vimKeys]
+    ++ [((mod4Mask       ,x),sendMessage $ Go (vimDir x)) | x <- vimKeys]
 
 customShiftMask = mod4Mask .|. shiftMask
 customCtrlMask  = mod4Mask .|. controlMask
@@ -55,14 +74,9 @@ main = do
                 , ppTitle = xmobarColor "green" "" . shorten 50
                 }
             , modMask = mod4Mask
+            , keys = customKeys deleteList keysList
             , borderWidth = 2
             , terminal = "urxvt"
             , normalBorderColor = "#333333"
             , focusedBorderColor = "#FFAA00"
             }
-
-            -- Remove bindings to keys so they can be reassigned
-            -- TODO: switch this to 'removeKeys' once the package is updated
-            -- with the bugfix.
-            `removeKeysFixed` [(i,j)|i<-[mod4Mask,customShiftMask],j<-vimKeys]
-            `additionalKeys` keysList
