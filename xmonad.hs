5ff63b32062b109104bc85300af8d81adaa994d6
Rebecca Skinner <rebecca@rebeccaskinner.net>
2011-09-20 21:38:47 -0500

    mirror keys works

--- xmonad.hs
+++ xmonad.hs
@@ -1,4 +1,5 @@
 import XMonad
+import XMonad.Operations
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
@@ -23,9 +24,8 @@ wideLayout = named "Wide" $ Mirror rzTall
 
 myLayout = fullLayout ||| tallLayout ||| mirrorTall ||| wideLayout
 
-layoutName :: XConfig Layout -> String
-layoutName l 
-    | l == (defaultConfig l fullLayout) = "Full"
+curLayout :: X String
+curLayout = gets windowset >>= return . description . W.layout . W.workspace . W.current
 
 vimDir k
     | k == xK_h = L
@@ -45,9 +45,12 @@ vimResizeMapMirror k
     | k == xK_k = sendMessage $ MirrorExpand
     | k == xK_l = sendMessage $ Shrink
 
-getVimResizeMap l
-    | (layoutName l) == "Mirror" = vimResizeMapMirror
-    | otherwise                  = vimResizeMap
+vimGetResizeMap k = do
+    str <- curLayout
+    case str of
+        "Mirror"  -> vimResizeMapMirror k
+        otherwise -> vimResizeMap k
+    return ()
 
 vimKeys = [xK_h, xK_j, xK_k, xK_l]
 
@@ -55,7 +58,7 @@ deleteList _ = [(i,j)|i<-[mod4Mask,customShiftMask,customCtrlMask],j<-vimKeys]
 
 keysList l = 
        [((customShiftMask,x),sendMessage $ Swap (vimDir x))|x<-vimKeys]
-    ++ [((customCtrlMask ,x),(getVimResizeMap l) x) | x <- vimKeys]
+    ++ [((customCtrlMask ,x),vimGetResizeMap x) | x <- vimKeys]
     ++ [((mod4Mask       ,x),sendMessage $ Go (vimDir x)) | x <- vimKeys]
 
 customShiftMask = mod4Mask .|. shiftMask
